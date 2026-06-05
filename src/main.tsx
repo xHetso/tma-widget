048499cdab3a8f5a6b8767a28e995ed52c63be0b
Hetso <data23491248@gmail.com>
2026-06-05 12:42:58 +0500

    Update tonconnect manifest and main component: Changed the manifest URL, name, and icon for the TMA Widget. Integrated TonConnectUIProvider in main.tsx to enhance the app's connection capabilities.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { TonConnectUIProvider } from '@tonconnect/ui-react'
 import './index.css'
 import App from './App.tsx'
 
+const manifestUrl = 'https://tma-widget.vercel.app/tonconnect-manifest.json'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <TonConnectUIProvider manifestUrl={manifestUrl}>
+      <App />
+    </TonConnectUIProvider>
   </StrictMode>,
 )
